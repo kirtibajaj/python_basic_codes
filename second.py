779b8efa43084f41b310e561ef20480a26d1969c
KIRTI Bajaj <32738647+kirtibajaj@users.noreply.github.com>
2018-06-13 18:35:03 +0530

    Update second.py
    added some comments on second.py

--- second.py
+++ second.py
@@ -1,6 +1,7 @@
 x=5
 y=6.25
 z=3.14+6.5j
+#this is to understand numbers in python
 print("type of x:",type(x))
 print("type of y:",type(y))
 print("typr of z:",type(z))
@@ -8,3 +9,7 @@ print("value of x:",x)
 print("Value of y:",y)
 print("Value of z:",z)
 print("changed")
+n = input()
+print("Always python take input as string")
+print(type(n))
+print(n)
